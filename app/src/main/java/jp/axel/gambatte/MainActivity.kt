aaeaa9d0d7dc78fd6e55e345dbde5a29bed8a1d3
Axel358 <edgar.serrano.9803@gmail.com>
2024-11-23 18:48:36 -0500

    feat: Group main buttons

--- app/src/main/java/jp/axel/gambatte/MainActivity.kt
+++ app/src/main/java/jp/axel/gambatte/MainActivity.kt
@@ -4,11 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.google.android.material.button.MaterialButtonToggleGroup
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mainBtnGroup: MaterialButtonToggleGroup = findViewById(R.id.main_btn_group)
+
+        mainBtnGroup.addOnButtonCheckedListener { _, _, _ ->
+            mainBtnGroup.clearChecked()
+        }
     }
 
     fun kanjiList(view: View) {
